51bc1fa6dbb8f0f1309b3bb8b6a4c7fd36d15af2
Ryan Kimpton <RyanKimpton965@gmail.com>
2019-11-29 11:38:59 +0000

    added DTO

--- src/main/java/DTO/ResponseTokenDTO.java
+++ src/main/java/DTO/ResponseTokenDTO.java
@@ -1,4 +1,23 @@
 package DTO;
 
 public class ResponseTokenDTO {
+
+    private String username;
+    private String bearerToken;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getBearerToken(){
+        return bearerToken;
+    }
+
+    public void setBearerToken(String bearerToken){
+        this.bearerToken = bearerToken;
+    }
 }
--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import DTO.RequestAccountDTO;
+import DTO.ResponseAccountDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 public interface AccountController {
+
+    @RequestMapping(path = "/account/{username}", method = {RequestMethod.GET})
+    public Boolean checkUsernameAvailable(@PathVariable String username);
+
+    @RequestMapping(path = "/account", method = {RequestMethod.POST})
+    public ResponseAccountDTO createAccount(@RequestBody RequestAccountDTO requestAccount);
+
+    @RequestMapping(path = "/account", method = {RequestMethod.PUT})
+    public Boolean authenticate(@RequestBody RequestAccountDTO requestAccount);
+
+    @RequestMapping(path = "/account", method = {RequestMethod.DELETE})
+    public String deleteAccount(@RequestBody RequestAccountDTO requestAccount);
 
 }
